4ef9a483122aaaa1e08a4d3495919eb33ff2e6ee
Rodrigo <rodrigo@cityhubs.io>
2022-02-09 16:35:41 -0300

    Container component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import useFormulario from "./hooks/useFormulario";
 import Input from "./components/Input";
 import Card from "./components/Card";
+import Container from "./components/Container";
 
 function App() {
   const [form, handleChange] = useFormulario({ name: "", lastName: "" });
 
   console.log(form);
   return (
-    <Card>
-      <form>
-        <Input
-          label="Nombre"
-          name="name"
-          value={form.name}
-          onChange={handleChange}
-        />
-        <Input
-          label="Apellido"
-          name="lastName"
-          value={form.lastName}
-          onChange={handleChange}
-        />
-      </form>
-    </Card>
+    <Container>
+      <Card>
+        <div style={{ padding: 20 }}>
+          <form>
+            <Input
+              label="Nombre"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+            />
+            <Input
+              label="Apellido"
+              name="lastName"
+              value={form.lastName}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+      </Card>
+    </Container>
   );
 }
 
